ed044ea6723ce2b6a0c071703e592de066d72567
Opilane TTHK <opilane@tthk.ee>
2020-10-12 14:47:40 +0300

    Create Web Page

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Week7Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your year of birth");
+            string UserInput = Console.ReadLine();
+            AgeCalc(UserInput);
+        }
+
+        public static void AgeCalc(string yearOfBirth)
+        {
+            int UserYearOfBirth = Int32.Parse(yearOfBirth);
+            Console.WriteLine($"You are {2020 - UserYearOfBirth} years old!");
+        }
+
+    }
+}
